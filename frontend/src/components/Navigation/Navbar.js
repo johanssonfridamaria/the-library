6641b931b1bbcfb2120e3033c8741f5cb9ac730e
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-20 14:37:18 +0100

    added navbar with logo, navigation and admin dropdown

--- frontend/src/components/Navigation/Navbar.js
+++ frontend/src/components/Navigation/Navbar.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" exact to="/">THE LIBRARY</NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-mdb-toggle="collapse"
+          data-mdb-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="fas fa-bars"></i>
+        </button>
+        <div className="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink className="nav-link" exact to="/">Home</NavLink>
+            <NavLink className="nav-link" exact to="/books">Library</NavLink>
+            <NavLink className="nav-link" exact to="/categories">Categories</NavLink>
+          </div>
+          {/* <ul class="navbar-nav"> */}
+          <div class="navbar-nav dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              // id="navbarDropdownMenuLink"
+              role="button"
+              data-mdb-toggle="dropdown"
+              aria-expanded="false"
+            > <i class="fas fa-user me-2"></i>
+                Admin
+            </a>
+            <div class="dropdown-menu">
+              <NavLink class="dropdown-item m-0" exact to="/signin">Sign in</NavLink>
+              <p class="dropdown-item m-0">Sign out</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
