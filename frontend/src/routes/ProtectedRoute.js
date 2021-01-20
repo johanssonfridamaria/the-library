3972dc47435555a5887b5571999b7435777c40bd
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-20 15:21:02 +0100

    added contional rendering depending on if isAuth is true

--- frontend/src/routes/ProtectedRoute.js
+++ frontend/src/routes/ProtectedRoute.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
-export const ProtectedRoute = ({component: Component, ...rest}) => {
+export const ProtectedRoute = ({ component: Component, ...rest }) => {
 
-
+const isAuth = useSelector(state => state.auth.isAuthenticated)
 
   return (
     <Route {...rest} render={props => {
+      if (isAuth) {
+        return <Component {...props} />
+      } else {
+        return <Redirect to={{ pathname: '/signin', state: { from: props.location } }} />
+      }
     }} />
   )
 
