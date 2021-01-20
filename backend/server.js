2aa2342ef32f4838a06d61cf9545ff930c8a334e
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-20 16:53:18 +0100

    connected with mongoose

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,10 @@ const serverURI='http://localhost:'+port;
 const mongoURI=process.env.MONGO_URI;
 
 app.listen(port, () => console.log('Webserver running at:' +serverURI));
+
+mongoose
+.set('useCreateIndex', true)
+.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => console.log('Connected to DB'))
