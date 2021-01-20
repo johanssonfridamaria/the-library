9b80abfe27bc7006d9dc68fe348d1f56f21c7d96
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-20 15:55:28 +0100

    added routes to books and categories views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ProtectedRoute } from './routes/ProtectedRoute'
 import Navbar from './components/Navigation/Navbar';
 import Footer from './components/Navigation/Footer';
 import SignIn from './views/Admin/SignIn';
+import Books from './views/Books';
+import Categories from './views/Categories';
 import LandingPage from './views/LandingPage';
 import Mangage from './views/Manage';
-import { ProtectedRoute } from './routes/ProtectedRoute'
 
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
       <Switch >
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/books" component={Books} />
+        <Route exact path="/categories" component={Categories} />
         <ProtectedRoute exact path="/manage" component={Mangage} />
       </Switch>
 
