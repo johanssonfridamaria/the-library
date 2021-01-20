06239e12cb51585e9e7293d2a431a02a02663ab5
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-20 15:34:33 +0100

    import protectedRoute and added correct folder to signin import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,25 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navigation/Navbar';
 import Footer from './components/Navigation/Footer';
-import SignIn from './components/Navigation/Admin/SignIn';
+import SignIn from './views/Admin/SignIn';
 import LandingPage from './views/LandingPage';
-import Mangage from './views/Mangage';
+import Mangage from './views/Manage';
+import { ProtectedRoute } from './routes/ProtectedRoute'
 
 
 function App() {
   return (
     <BrowserRouter >
-    <Navbar />
+      <Navbar />
 
 
-    <Switch >
-      <Route exact path="/" component={LandingPage}/>
-      <Route exact path="/signin" component={SignIn}/>
-      <ProtectedRoute exact path="/manage" component={Mangage}/>
-    </Switch>
+      <Switch >
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/signin" component={SignIn} />
+        <ProtectedRoute exact path="/manage" component={Mangage} />
+      </Switch>
 
-    <Footer />
+      <Footer />
     </BrowserRouter>
   );
 }
