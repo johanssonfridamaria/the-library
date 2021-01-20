fe2f339303327a19af5c8c134e598f8714848580
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-20 14:36:31 +0100

    added switch and route to home and signin, imported signin view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navigation/Navbar';
 import Footer from './components/Navigation/Footer';
+import SignIn from './components/Navigation/Admin/SignIn';
 
 
 function App() {
@@ -9,6 +10,11 @@ function App() {
     <BrowserRouter >
     <Navbar />
 
+
+    <Switch >
+      <Route exact path="/" component={}/>
+      <Route exact path="/signin" component={SignIn}/>
+    </Switch>
 
     <Footer />
     </BrowserRouter>
