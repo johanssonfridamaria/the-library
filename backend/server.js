06422535718d26eaf3a8fbbc8bf228df494e3e70
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-20 16:43:11 +0100

    added serverURI and mongoURI to CB

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,7 @@ const mongoose = require('mongoose');
 
 const port = process.env.port || 8000;
 require('dotenv').config();
+
+const serverURI='http://localhost:'+port;
+const mongoURI=process.env.MONGO_URI;
+
