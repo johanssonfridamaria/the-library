7efb035c0427bc779e5db1cc1ec45a4e14a87cd5
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-20 16:44:03 +0100

    added app.listen

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,4 @@ require('dotenv').config();
 const serverURI='http://localhost:'+port;
 const mongoURI=process.env.MONGO_URI;
 
+app.listen(port, () => console.log('Webserver running at:' +serverURI));
