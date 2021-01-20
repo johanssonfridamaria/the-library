da89fc342881d79a1c73781d87156bc8d9e3ec65
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-20 13:18:48 +0100

    imported navbar and footer comp to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navigation/Navbar';
+import Footer from './components/Navigation/Footer';
+
 
 function App() {
   return (
     <BrowserRouter >
+    <Navbar />
+
+
+    <Footer />
     </BrowserRouter>
   );
 }
