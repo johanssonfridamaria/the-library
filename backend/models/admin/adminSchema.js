edc542f3c22349e1e70c7d08cd3bafd049d69cbb
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-21 15:44:55 +0100

    created mongodb Schema

--- backend/models/admin/adminSchema.js
+++ backend/models/admin/adminSchema.js
@@ -1,2 +1,12 @@
 const mongo = require('mongoose');
 
+const adminSchema = mongodb.Schema({
+
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  passwordHash: {type: String, required: true},
+
+  created: {type: Date, default: Date.now()},
+  modified: {type: Date, default: Date.now()},
+});
