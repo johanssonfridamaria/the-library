af2173b9ba104432b22478e321fcfb16b0299839
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-21 10:52:26 +0100

    exported generated token and function to verify token

--- backend/authentication/auth.js
+++ backend/authentication/auth.js
@@ -3,3 +3,21 @@ require('dotenv/config');
 
 const secretKey = process.env.SECRET_KEY;
 
+exports.generateToken = id => {
+  return jwt.sign({id:id }, secretKey, { expiresIn: '3h'})
+};
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    req.userData = jwt.verify(token, secretKey);
+    next();
+  } 
+  catch {
+    return res.status(401).json({
+      statusCode: 401,
+      status: false,
+      message: 'Access restricted! Please signin!'
+    });
+  }
+}
