ed53af00a455e737a79bc3fb506e302df95c0212
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-22 16:51:39 +0100

    added categories controller

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 const adminController = require('./controllers/adminController')
+const categoriesController = require('./controllers/categoriesController')
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
@@ -18,5 +19,6 @@ app.use(express.json());
 
 
 app.use('/api/admin', adminController);
+app.use('/api/categories', categoriesController);
 
 module.exports = app;
