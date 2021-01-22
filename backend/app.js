ca66c53d8a10e8ea356a390a2735cb63696817a9
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-22 12:40:32 +0100

    added adminController

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const adminController = require('./controllers/adminController')
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization, Origin, X-Requested-With")
@@ -13,5 +15,8 @@ app.use((req, res, next) => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+app.use('/api/admin', adminController);
 
 module.exports = app;
