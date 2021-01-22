0614bb7b4c0716826e7c73d1bcad77535c26f810
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-22 19:49:16 +0100

    imported auth

--- backend/controllers/categoriesController.js
+++ backend/controllers/categoriesController.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 const categoryModel = require('../models/categories/categoryModel');
+const auth = require('../authentication/auth')
 
 //get categories
 router.get('/', categoryModel.getCategories);
 //get one category
 router.get('/:id', categoryModel.getOneCategory);
 //create a new category
-router.post('/new', categoryModel.checkCategoryName, categoryModel.createCategory);
+router.post('/new' ,categoryModel.checkCategoryName, categoryModel.createCategory);
 //update specific category
 router.patch('/:id', categoryModel.checkCategoryName, categoryModel.updateCategory);
 //delete specific category
