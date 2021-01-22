c925525884438ebb58070cfa272ac8c9ca04c165
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-22 15:59:51 +0100

    added functions for login, loginSuccess, loginFailed and logout

--- frontend/src/store/actions/authenticate.js
+++ frontend/src/store/actions/authenticate.js
@@ -1,5 +1,43 @@
+import axios from '../../axios';
 import actiontypes from '../actiontypes';
 
-export const login = () => {
-  
+export const login = (email, password) => {
+  return async dispatch => {
+    const admin = {
+      email,
+      password
+    }
+    await axios.post('/admin/login', admin)
+      .then(res => {
+        console.log(res);
+
+        if (res.status === 200) {
+          localStorage.setItem('token', res.data.token);
+          dispatch(loginSuccess(true))
+        } else {
+          return dispatch(loginFailed(false))
+        }
+      })
+  }
+}
+
+export const loginSuccess = (payload) => {
+  return {
+    type: actiontypes().auth.loginSuccess,
+    payload
+  }
+}
+
+export const loginFailed = (payload) => {
+  return {
+    type: actiontypes().auth.loginFailed,
+    payload
+  }
+}
+
+export const logout = () => {
+  return {
+    type: actiontypes().auth.logout,
+    payload: false
+  }
 }
