61365976572ef60d809dbd9f2fec0948cb32efea
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-22 16:07:04 +0100

    created mongodb schema and exported it

--- backend/models/categories/categorySchema.js
+++ backend/models/categories/categorySchema.js
@@ -1,1 +1,13 @@
 const mongodb = require('mongoose');
+const ObjectId = mongodb.Schema.Types.ObjectId;
+
+const categorySchema = mongodb.Schema({
+
+  _id: ObjectId,
+  name: { type: String, required: true, unique: true, },
+
+  created: { type: Date, default: Date.now() },
+  modified: { type: Date, default: Date.now() },
+});
+
+module.exports = mongodb.model('Category', categorySchema);
