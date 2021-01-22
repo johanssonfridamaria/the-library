8d9e33b2c75fa2782597330cde8d985918efaed1
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-22 13:30:53 +0100

    added login admin controller

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -4,5 +4,7 @@ const adminModel = require('../models/admin/adminModel');
 // register admin
 
 router.post('/register', adminModel.registerAdmin);
+router.post('/login', adminModel.loginAdmin);
+// router.get('/login', adminModel.loginAdmin);
 
 module.exports = router;
